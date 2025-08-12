c2f5437e8755a8ba10926c4ccf0f0bf7320719d8
Steff1928 <prossersteve151@gmail.com>
2025-08-12 22:55:31 +1200

    Added chat interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emergency_management_system/pages/chat.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello'),
+          child: Chat(),
         ),
       ),
     );
